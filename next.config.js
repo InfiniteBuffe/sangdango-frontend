4c2e6f0203d7ac0cc8a13f14c79d81db47c4429c
박태진 <blogmangamer@naver.com>
2023-01-17 12:03:24 +0000

    next export 오류 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '/',
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
