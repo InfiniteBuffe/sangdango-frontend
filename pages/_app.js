0e4e4b3bc251eb5c2423e0258540cc91e9e58665
박태진 <blogmangamer@naver.com>
2023-01-21 11:23:50 +0000

    메인 화면 메뉴 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import '@/styles/globals.css'
+import { Toaster } from 'react-hot-toast'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Toaster position="bottom-center"
+        reverseOrder={false}
+        toastOptions={{
+          style: {
+            borderRadius: '30px',
+            background: '#333',
+            color: '#fff',
+          },
+        }}
+        />
+      <Component {...pageProps} />
+    </>
+  )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css" />
+
       </Head>
       <body>
         <Main />
