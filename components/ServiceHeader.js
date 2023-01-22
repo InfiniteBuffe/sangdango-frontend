cc8dcc4764862c588b4b1472e759eb21bbe962e4
박태진 <blogmangamer@naver.com>
2023-01-22 09:25:11 +0000

    오류 수정 및 ServiceHeader 추가

--- components/ServiceHeader.js
+++ components/ServiceHeader.js
@@ -3,7 +3,12 @@ import styles from '@/styles/components/ServiceHeader/ServiceHeader.module.css'
 const ServiceHeader = () => {
     return (
         <>
-            
+            <div className={styles.header}>
+                <div className={styles.title}>
+                    동아리
+                </div>
+            </div>
+            <div className={styles.header_space} />
         </>
     )
 }
