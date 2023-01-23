2e820e4161120587787142244700dacaa3067d9a
박태진 <blogmangamer@naver.com>
2023-01-23 17:19:21 +0000

    클라우드플레어 설정

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+export const config = {
+  runtime: 'experimental-edge',
+}
+
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
