b3385a7a1fc8f6556635b8a1a561383c633e6987
박태진 <blogmangamer@naver.com>
2023-01-23 17:32:58 +0000

    배포 오류 수정

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export const config = {
-  runtime: 'experimental-edge',
-}
-
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
