c7fe6fe2b4ca61eff2fdd16ef43641533fb87e37
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2023-07-13 00:50:30 +0900

    Update home.js

--- pages/service/rental/home.js
+++ pages/service/rental/home.js
@@ -12,16 +12,16 @@ import Twemoji from 'react-twemoji'
 
 const Home = () => {
 
-    const [currentCount,setCurrentCount] = useState('-')
+    const [currentCount, setCurrentCount] = useState('-')
     const [formOpen, setFormOpen] = useState(false)
-    
-    useEffect(()=>{
-        const url = (process.env.NEXT_PUBLIC_ENV=='dev')?(process.env.NEXT_PUBLIC_DEV_URL):(process.env.NEXT_PUBLIC_PROD_URL)
+
+    useEffect(() => {
+        const url = (process.env.NEXT_PUBLIC_ENV == 'dev') ? (process.env.NEXT_PUBLIC_DEV_URL) : (process.env.NEXT_PUBLIC_PROD_URL)
         axios({
             url: url + '/api/rental/current',
             method: 'GET',
         })
-            .then(r=>{
+            .then(r => {
                 if (r.status != 200) {
                     setCurrentCount(0)
                 }
@@ -53,19 +53,19 @@ const Home = () => {
                     </div>
                 </div>
                 <div className={styles.warning}>
-                    <span id={styles.warning_strong}>ⓘ 참고해주세요.</span><br/>
-                    - 실시간으로 현재 수량이 표시되나, 새로고침 해야 갱신됩니다.<br/>
-                    - 잘못된 학번으로 신청 시 불이익이 있을 수 있으니 확인해주세요.<br/>
-                    - 타인의 정보를 무단으로 이용 후 적발 시 이용정지 됩니다.<br/>
+                    <span id={styles.warning_strong}>ⓘ 참고해주세요.</span><br />
+                    - 실시간으로 현재 수량이 표시되나, 새로고침 해야 갱신됩니다.<br />
+                    - 잘못된 학번으로 신청 시 불이익이 있을 수 있으니 확인해주세요.<br />
+                    - 타인의 정보를 무단으로 이용 후 적발 시 이용정지 됩니다.<br />
                     - 반납은 익일 점심시간 반납을 원칙으로 합니다.
                 </div>
-                <div className={styles.button} onClick={()=>setFormOpen(true)}>
+                <div className={styles.button} onClick={() => setFormOpen(true)}>
                     <div className={styles.button_text}>
                         대여 신청하기
                     </div>
                 </div>
                 <div className={styles.notice}>
-                    아직 신청을 할 수 없어요.<br/>곧 다시 만나요!
+                    아직 신청을 할 수 없어요.<br />곧 다시 만나요!
                 </div>
             </Container>
             <BottomSheet className={styles.bottom_sheet} open={formOpen}>
@@ -73,8 +73,8 @@ const Home = () => {
                     <Twemoji options={{ className: styles.emoji_font }}>☂</Twemoji>
                     &nbsp;대여 신청서
                 </div>
-                <TextField style={{width:'calc(100% - 40px)', marginLeft: '20px', marginTop: '20px'}} fullWidth label="학번" variant="outlined" />
-                <TextField style={{width:'calc(100% - 40px)', marginLeft: '20px', marginTop: '20px'}} fullWidth label="이름" variant="outlined" />
+                <TextField style={{ width: 'calc(100% - 40px)', marginLeft: '20px', marginTop: '20px' }} fullWidth label="학번" variant="outlined" />
+                <TextField style={{ width: 'calc(100% - 40px)', marginLeft: '20px', marginTop: '20px' }} fullWidth label="이름" variant="outlined" />
             </BottomSheet>
         </>
     )
