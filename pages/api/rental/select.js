f6f90b2e15cfc016499aa373b394b6918534c5e3
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2023-08-21 13:17:16 +0900

    Create select.js

---
+++ pages/api/rental/select.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+export default async function handler(req, res) {
+    if (req.method !== 'GET') {
+        res.status(405).send({ status:405, message: 'Method Not Allowed' })
+        return
+    }
+
+    const currentRental = await client.CurrentRental.findMany({})
+    let count = (currentRental).length
+    let data = []
+    for (let i=0;i<=count-1;i++) {
+        let _d = currentRental[`${i}`]
+        data.push({
+            date: _d.createdAt,
+            studentId: _d.studentId,
+            name: _d.name
+        })
+    }
+
+    res
+        .status(200)
+        .json(data)
+}
