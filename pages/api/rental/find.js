5f70654e73b6d13cf4919daf0585708fbb487196
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2023-08-29 13:00:25 +0900

    Update find.js

--- pages/api/rental/find.js
+++ pages/api/rental/find.js
@@ -23,45 +23,36 @@ export default async function handler(req, res) {
         }
     })
 
-    let kstDate = new Date(utcDate);
-    kstDate.setHours(kstDate.getHours() + 9);
-    if (kstDate.getUTCHours() >= 24) {
-        kstDate.setDate(kstDate.getDate() + 1);
+    const convertUTCtoKST = (utcDate) => {
+        let kstDate = new Date(utcDate);
+        kstDate.setHours(kstDate.getHours() + 9)
+        if (kstDate.getUTCHours() >= 24) {
+            kstDate.setDate(kstDate.getDate() + 1)
+        }
+        return kstDate
     }
-    return kstDate;
-}
 
-
-const formatDateTime = (dateTimeString) => {
-    let date = convertUTCtoKST(dateTimeString);
-    if (process.env.NEXT_PUBLIC_ENV == "prod") {
+    const formatDateTime = (dateTimeString) => {
+        // 서버에서는 시간이 다름.
+        let date = convertUTCtoKST(dateTimeString);
         let formattedDateTime =
             date.getFullYear() + "년 " +
             (date.getMonth() + 1) + "월 " +
             date.getDate() + "일 " +
-            (date.getHours() + 9) + "시 " +
+            date.getHours() + "시 " +
             date.getMinutes() + "분 " +
             date.getSeconds() + "초";
         return formattedDateTime;
     }
-    let formattedDateTime =
-        date.getFullYear() + "년 " +
-        (date.getMonth() + 1) + "월 " +
-        date.getDate() + "일 " +
-        date.getHours() + "시 " +
-        date.getMinutes() + "분 " +
-        date.getSeconds() + "초";
-    return formattedDateTime;
-}
 
-let data = {
-    message: 'success',
-    isListed: (currentRental[0] != undefined) ? true : false,
-}
-if (data.isListed) {
-    data.time = formatDateTime(currentRental[0].createdAt)
-}
-res
-    .status(200)
-    .json(data)
+    let data = {
+        message: 'success',
+        isListed: (currentRental[0] != undefined) ? true : false,
+    }
+    if (data.isListed) {
+        data.time = formatDateTime(currentRental[0].createdAt)
+    }
+    res
+        .status(200)
+        .json(data)
 }
