9a3d7d036e324972fb924f80af409136fda4a9e3
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2024-03-17 03:32:13 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,29 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID
 
 export default function Document() {
   return (
     <Html lang="ko">
       <Head>
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css" />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
+        />
       </Head>
       <body>
         <Main />
