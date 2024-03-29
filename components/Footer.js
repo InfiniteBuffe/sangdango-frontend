615e213f8fba99efdb186566a94e3727e3bd73a2
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2024-03-30 04:05:11 +0900

    디자인 오류 수정

--- components/Footer.js
+++ components/Footer.js
@@ -5,8 +5,7 @@ const Footer = () => {
     return (
         <>
             <div className={styles.footer}>
-                <Container>
-                    {/* <table className={styles.table}>
+                {/* <table className={styles.table}>
                         <tbody>
                             <tr>
                                 <td className={styles.name}>운영</td>
@@ -27,18 +26,17 @@ const Footer = () => {
                             </tr>
                         </tbody>
                     </table> */}
-                    <div className={styles.text}>
-                        운영: 상당고등학교 학생복지부
-                        <br />
-                        개발: 상당고등학교 25회 졸업생 박태진
-                        <br />
-                        호스팅 서비스 제공: Vercel, Vultr, Cloudflare
-                        <br />
-                        <div className={styles.github}>
-                            <a href='https://github.com/infinitebuffe/sangdango-frontend'>이 프로젝트</a>는 Github에 오픈소스로 공개되어 있습니다.
-                        </div>
+                <div className={styles.text}>
+                    운영: 상당고등학교 학생복지부
+                    <br />
+                    개발: 상당고등학교 25회 졸업생 박태진
+                    <br />
+                    호스팅 서비스 제공: Vercel, Vultr, Cloudflare
+                    <br />
+                    <div className={styles.github}>
+                        <a href='https://github.com/infinitebuffe/sangdango-frontend'>이 프로젝트</a>는 Github에 오픈소스로 공개되어 있습니다.
                     </div>
-                </Container>
+                </div>
             </div>
         </>
     )
